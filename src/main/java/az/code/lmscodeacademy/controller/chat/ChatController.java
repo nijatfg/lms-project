49a1dd642545bea304c2f603ceb370a4da8f73d5
nijatfg <153588579+nijatfg@users.noreply.github.com>
2024-04-07 22:17:57 +0400

    WebSockerConfig and ChatController cors added

--- src/main/java/az/code/lmscodeacademy/controller/chat/ChatController.java
+++ src/main/java/az/code/lmscodeacademy/controller/chat/ChatController.java
@@ -9,6 +9,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/chats")
+@CrossOrigin
 public class ChatController {
 
     private final SimpMessagingTemplate messagingTemplate;
