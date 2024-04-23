22fe77984e53ee5c71998f54068a99e8139f3470
nijatfg <153588579+nijatfg@users.noreply.github.com>
2024-04-23 15:42:47 +0400

    commit

--- src/main/java/az/code/lmscodeacademy/service/grade/GradeService.java
+++ src/main/java/az/code/lmscodeacademy/service/grade/GradeService.java
@@ -11,10 +11,12 @@ import az.code.lmscodeacademy.exception.users.UserNotFoundException;
 import az.code.lmscodeacademy.repository.grade.GradeRepository;
 import az.code.lmscodeacademy.repository.submission.SubmissionRepository;
 import az.code.lmscodeacademy.repository.user.UserRepository;
+import az.code.lmscodeacademy.service.email.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,6 +28,7 @@ public class GradeService {
     private final SubmissionRepository submissionRepository;
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final EmailService emailService;
 
     public GradeResponse gradeSubmission(Long submissionId, GradeRequest gradeRequest) {
         Submission submission = submissionRepository.findById(submissionId)
@@ -38,7 +41,20 @@ public class GradeService {
 
         Grade savedGrade = gradeRepository.save(grade);
 
+        sendGradeEmailToUser(submission.getUser(), grade);
+
         return modelMapper.map(savedGrade, GradeResponse.class);
+    }
+
+    private void sendGradeEmailToUser(User user, Grade grade) {
+        String subject = "📝 Grade Notification: " + grade.getSubmission().getAssignment().getTitle();
+        String content = "\n\n🌟 Dear " + user.getFirstName() + ",\n\nWe are pleased to inform you that your recent assignment has been graded.\n\n📚 Assignment Title: "
+                + grade.getSubmission().getAssignment().getTitle() + "\n\n🔢 Grade: " + grade.getScore() + "\n\n💬 Feedback:\n" + grade.getFeedback()
+                + "\n\nThank you for your effort and dedication in completing the assignment. If you have any questions or need further clarification, please feel free to reach out.\n\nBest regards" +
+                "\n\n<a href=\"http://localhost:3000/student/assignments\">Click here</a> to view the assignment in the student portal.";
+
+
+        emailService.sendEmail(subject, content, Collections.singletonList(user.getEmail()));
     }
 
     public List<GradeResponse> findAllGrades() {
